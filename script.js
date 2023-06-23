7552182b86f0b3f37e5cebd3129da885be58baab
Aanchal Anand <120234249+AanchalAnand@users.noreply.github.com>
2023-06-23 20:08:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // complete the given function
 
 function palindrome(str){
-
+  str=str.replaceAll(' ','');
+	let newstr=str.reverse();
+	if(str==newstr) return true;
+	return false;
 }
 module.exports = palindrome
