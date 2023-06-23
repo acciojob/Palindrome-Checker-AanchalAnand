0dc6c7ef05e22a1a456ca36faab89e86bc215c47
Aanchal Anand <120234249+AanchalAnand@users.noreply.github.com>
2023-06-23 20:17:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ function palindrome(str){
   lrt s=str.replaceAll(' ','');
 	let newstr=s.reverse();
 	for(let i=0;i<s.length;i++){
-	if(s[i]==newstr[i]) return true;
-	return false;
+	if(s[i]!=newstr[i]) return false;
+
 	}
+	return true;
 }
 module.exports = palindrome
