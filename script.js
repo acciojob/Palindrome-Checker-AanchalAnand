86c33416903c59e061ef7ca203140c3b2b252802
Aanchal Anand <120234249+AanchalAnand@users.noreply.github.com>
2023-06-23 20:33:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let n=str.length-1;
 	let i=0;
 	str=str.toLowerCase();
 	while(i<n){
-	if(str.charAt(i)!=str.charAt(n--)) return false;
+	if(str.charAt(i++)!=str.charAt(n--)) return false;
 
 	}
 	return true;
